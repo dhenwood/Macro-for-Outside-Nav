d12f2dd65292533f0c5f51d7ea0f82e89bb04fd4
dhenwood <dhenwood@users.noreply.github.com>
2025-08-05 12:22:46 +1000

    Create messageSpacesMacro.js

---
+++ messageSpacesMacro.js
@@ -0,0 +1,136 @@
+import xapi from 'xapi';
+
+const botToken = "setBotToken"
+const deviceId = "setDeviceId"
+const spacesUrl = "setCiscoSpacesUrl"
+
+function sendMessage(message) {
+  const url = "https://webexapis.com/v1/xapi/command/UserInterface.Message.Alert.Display"
+  const authorizationHeader = "Authorization: Bearer " + botToken;
+  const body = {
+    "deviceId": deviceId,
+    "arguments": {
+      "Duration": 10,
+      "Target": "OSD",
+      "Text": "Message from outside room",
+      "Title": message
+    }
+  };
+
+  xapi.command('HttpClient Post', {
+    'Header': authorizationHeader,
+    'Url': url,
+    'AllowInsecureHTTPS': 'False'
+  }, JSON.stringify(body)).then(
+    (result) => {
+      console.log("result: " + result)
+    })
+}
+
+function init() {
+  xapi.Config.WebEngine.Features.Peripherals.WebGL.set("On");
+  xapi.Config.HttpClient.Mode.set("On");
+  setupListerners();
+  createPanels();
+}
+
+function setupListerners() {
+  xapi.event.on('UserInterface Extensions Widget Action', (event) => {
+    if (event.WidgetId == 'button1' && event.Type == 'pressed') {
+      console.log("button1")
+      const message = "Can you pop out?"
+      sendMessage(message);
+    } else if (event.WidgetId == 'button2' && event.Type == 'pressed') {
+      console.log("button2")
+      const message = "I have this room booked"
+      sendMessage(message);
+    } else if (event.WidgetId == 'button3' && event.Type == 'pressed') {
+      console.log("button3")
+      const message = "Will you be long?"
+      sendMessage(message);
+    } else if (event.WidgetId == 'button4' && event.Type == 'pressed') {
+      console.log("button4")
+      const message = "Free coffee out here!"
+      sendMessage(message);
+    }
+  })
+}
+
+function createPanels() {
+  const messageXml = `
+  <Extensions>
+  <Version>1.11</Version>
+  <Panel>
+    <Order>1</Order>
+    <Origin>local</Origin>
+    <Location>RoomScheduler</Location>
+    <Icon>Microphone</Icon>
+    <Color>#262626</Color>
+    <Name>Message Inside</Name>
+    <ActivityType>Custom</ActivityType>
+    <Page>
+      <Name>Send Message</Name>
+      <Row>
+        <Name>Send message to</Name>
+        <Widget>
+          <WidgetId>button1</WidgetId>
+          <Name>Can you pop out?</Name>
+          <Type>Button</Type>
+          <Options>size=2</Options>
+        </Widget>
+        <Widget>
+          <WidgetId>button2</WidgetId>
+          <Name>I've this room booked</Name>
+          <Type>Button</Type>
+          <Options>size=2</Options>
+        </Widget>
+      </Row>
+      <Row>
+        <Name/>
+        <Widget>
+          <WidgetId>button3</WidgetId>
+          <Name>Will you be long?</Name>
+          <Type>Button</Type>
+          <Options>size=2</Options>
+        </Widget>
+        <Widget>
+          <WidgetId>button4</WidgetId>
+          <Name>Free coffee out here!</Name>
+          <Type>Button</Type>
+          <Options>size=2</Options>
+        </Widget>
+      </Row>
+      <Row>
+        <Name>Row</Name>
+        <Widget>
+          <WidgetId>widget_1</WidgetId>
+          <Name>The corresponding message will be displayed on the screen inside the meeting room.</Name>
+          <Type>Text</Type>
+          <Options>size=4;fontSize=small;align=center</Options>
+        </Widget>
+      </Row>
+      <Options>hideRowNames=1</Options>
+    </Page>
+  </Panel>
+</Extensions>
+  `
+  const spacesXml = `
+  <Extensions>
+  <Version>1.11</Version>
+  <Panel>
+    <Order>2</Order>
+    <Origin>local</Origin>
+    <Location>RoomScheduler</Location>
+    <Icon>Custom</Icon>
+    <Name>Cisco Spaces</Name>
+    <ActivityType>WebApp</ActivityType>
+    <ActivityData>${spacesUrl}</ActivityData>
+  </Panel>
+</Extensions>
+  `
+
+  xapi.Command.UserInterface.Extensions.Panel.Save({PanelId: "messagesPanel"}, messageXml);
+  xapi.Command.UserInterface.Extensions.Panel.Save({PanelId: "spacesPanel"}, spacesXml);
+}
+
+init();
